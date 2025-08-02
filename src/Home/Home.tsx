cb0ef12ba60f98e29d4072e4e734e733b71abda7
MohammadJewelRana <js.rana0326@gmail.com>
2025-08-02 12:40:48 +0600

    lottielies animation add in coming soon

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import logo from "../assets/logo/logo.png";
 import CountDown from "./CountDown";
+import ComingSoon from "./Coming";
 
 const Home = () => {
   return (
@@ -18,17 +19,25 @@ const Home = () => {
         {/* Main Content Area */}
         <div className="flex-grow flex flex-col justify-center items-center  ">
           <div className=" text-center py-8 md:py-16">
-            <h2 className="text-5xl md:text-[120px] font-bold uppercase text-gray-800 mb-4 leading-snug">
+            {/* <h2 className="text-5xl md:text-[120px] font-bold uppercase text-gray-800 mb-4 leading-snug">
               Coming Soon
-            </h2>
-            <p className="  text-lg md:text-[28px] text-gray-600 max-w-4xl mx-auto mb-8 leading-relaxed font-bold">
+            </h2> */}
+
+            <ComingSoon/>
+            {/* <h2 className="text-5xl md:text-[120px] font-bold uppercase text-gray-800 mb-4 leading-snug">
+              Coming Soon
+            </h2> */}
+            <p className="px-2  text-lg md:text-[28px] text-gray-600 max-w-4xl mx-auto mb-8 leading-relaxed font-bold">
               We’re building Bangladesh’s most customer-loved <br />
-              <span className="text-[#E26626] md:text-[32px]"> Gadget & Gift</span>{" "}
+              <span className="text-[#E26626] md:text-[32px]">
+                {" "}
+                Gadget & Gift
+              </span>{" "}
               destination.
             </p>
 
             {/* Countdown */}
-      
+
             <CountDown />
           </div>
           {/* Social Icons */}
